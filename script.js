2fbad77e51692bcf3b30eb342b372679ee2a2d3e
Nicci Moyer <niccihendrix@yahoo.com>
2020-08-04 10:01:54 -0500

    translateAPI

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
-	"method": "POST",
-	"headers": {
-		"x-rapidapi-host": "google-translate1.p.rapidapi.com",
-		"x-rapidapi-key": "241e327413mshff46d0a3fc5647ep185311jsne86067773ada",
-		"accept-encoding": "application/gzip",
-		"content-type": "application/x-www-form-urlencoded"
-	},
-	"data": {
-		"source": "en",
-		"q": "Hello, world!",
-		"target": "es"
-	}
-}
+$("translateIt").on("click", function(event){
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+	event.preventDefault();
+
+	var enText = $("#textArea").val().trim();
+	
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
+		"method": "POST",
+		"headers": {
+			"x-rapidapi-host": "google-translate1.p.rapidapi.com",
+			"x-rapidapi-key": "241e327413mshff46d0a3fc5647ep185311jsne86067773ada",
+			"accept-encoding": "application/gzip",
+			"content-type": "application/x-www-form-urlencoded"
+		},
+		"data": {
+			"source": "en",
+			"q": "Hello, world!",
+			"target": "es"
+		}
+	}
+
+	$.ajax(settings).done(function (response) {
+		// console.log(response);
+		let esText = $("newText").text(respone.data.translations[0].translatedText)
+		textArea.append(esText)
+
+	});
+
+})
+
